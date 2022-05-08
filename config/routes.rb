4e89bb4958b034c9e57dd14897c315eb29a07dd4
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-08 17:25:01 +0000

    [Add]モデルとコントローラーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
+  get "/" => "homes#top"
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+  # get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+   resources :books
 end
+
+                              #     Prefix Verb   URI Pattern                                                                                       Controller#Action
+                              #     books GET    /books(.:format)                                                                                  books#index
+                              #           POST   /books(.:format)                                                                                  books#create
+                              #   new_book GET    /books/new(.:format)                                                                              books#new
+                              # edit_book GET    /books/:id/edit(.:format)                                                                         books#edit
+                              #       book GET    /books/:id(.:format)                                                                              books#show
+                              #           PATCH  /books/:id(.:format)                                                                              books#update
+                              #           PUT    /books/:id(.:format)                                                                              books#update
+                              #           DELETE /books/:id(.:format)                                                                              books#destroy
