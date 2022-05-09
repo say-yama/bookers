4e974a0188233a745615c1f159dff1df44ddac57
EC2 Default User <ec2-user@ip-172-31-47-176.ec2.internal>
2022-05-09 03:15:34 +0000

    [Add]新規投稿bodytable

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(list_params)
+    book.save
+    redirect_to "/"
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
   end
 end
